313f6f288753b73a49f5f3e68a5dd99fe0977296
Hochan Kim <danielhochan1@gmail.com>
2023-10-06 21:10:08 +0900

    Feat: WishView main/detail

--- wishes/serializers.py
+++ wishes/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from wishes.models import Wish
 
+
 class WishCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wish
         fields = ("title", "wish_name", "content", "wish_img")
 
+
+class WishSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+
+    def get_author(self, obj):
+        return obj.author.email
+
+    class Meta:
+        model = Wish
+        fields = "__all__"
