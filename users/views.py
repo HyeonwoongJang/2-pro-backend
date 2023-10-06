c4208ad8a1a727d65a0677a75a285caf6ecb348d
Hochan Kim <danielhochan1@gmail.com>
2023-10-06 13:22:44 +0900

    feat: follow view

--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
-from users.models import User
+from user.models import User
+
 from users.serializers import UserSerializer
 
 
@@ -20,12 +21,16 @@ class SignupView(APIView):
 
 class FollowView(APIView):
     def post(self, request, user_id):
-        """사용자는 다를 사용자를 팔로우하거나 언팔로우하는 기능입니다."""
-        follower = get_object_or_404(User, id=user_id)
-        followee = request.user
-        if followee in follower.followers.all():
-            follower.followers.remove(followee)
-            return Response("언팔로우 했습니다.", status=status.HTTP_200_OK)
+        """사용자가 다른 사용자를 팔로우하고 언팔로우합니다."""
+
+        # user_id 사용해서 팔로우 사용자 가져오기
+        followee = get_object_or_404(User, id=user_id)
+        follower = request.user  # 팔로우 요청을 보내는 사용자
+        if follower in follower.followers.all():  # 사용자를 이미 팔로우한다면
+            # followee 사용자 목록에서 follower remove로 제거하기
+            followee.followers.remove(follower)
+            return Response("unfollow 했습니다.", status=status.HTTP_200_OK)
         else:
-            follower.followers.add(followee)
-            return Response("팔로우 했습니다.", status=status.HTTP_200_OK)
+            # 아니면 followee 사용자 목록에 follower 추가
+            followee.followers.add(follower)
+            return Response("follow 했습니다.", status=status.HTTP_200_OK)
