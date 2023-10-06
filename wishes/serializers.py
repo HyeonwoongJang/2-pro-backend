226747e48a2de3ee8b221c315fcce1f113b4b8b7
Myeongzip <shayne0914@skuniv.ac.kr>
2023-10-06 21:06:53 +0900

    Feat: Comment Create and Delete

--- wishes/serializers.py
+++ wishes/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from wishes.models import Wish
+from wishes.models import Comment, Wish
 
 class WishCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wish
         fields = ("title", "wish_name", "content", "wish_img")
 
+
+      
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ("content",)
+        
--- wishes/views.py
+++ wishes/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.response import Response
-from wishes.serializers import WishCreateSerializer
+from rest_framework.generics import get_object_or_404
+from wishes.models import Comment, Wish
+from wishes.serializers import CommentSerializer, WishCreateSerializer
 
 class WishView(APIView):
     def get(self, request, wish_id=None):
@@ -30,11 +32,28 @@ class WishView(APIView):
         """지정된 위시를 삭제합니다."""
 
 class CommentView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     def post(self, request, wish_id):
         """wish_id를 받아 해당 위시에 댓글을 생성합니다."""
-        
-    def delete(self, request, comment_id):
-        """지정된 댓글을 삭제합니다.""" 
+        serialzer = CommentSerializer(data=request.data)
+        if serialzer.is_valid():
+            serialzer.save(author=request.user, wish_id=wish_id)
+            return Response(serialzer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serialzer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+
+
+    def delete(self, request, wish_id, comment_id):
+        """지정된 댓글을 삭제합니다."""
+        comment = get_object_or_404(Comment, id=comment_id)
+        # print(comment)
+        if not request.user == comment.author:
+            return Response({"message":"권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+        else:
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
 class LikeView(APIView):
     def post(self, request, wish_id):
