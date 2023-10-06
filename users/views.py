edd5e01edba2e26120a150669addfd82acc2ef70
Myeongzip <shayne0914@skuniv.ac.kr>
2023-10-06 14:19:27 +0900

    Fix: Add Missing ProfileView_get and Fix FollowView Typo

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from users.models import User
 from rest_framework_simplejwt.views import TokenObtainPairView
-from users.serializers import UserSerializer
+from users.serializers import LoginSerializer, ProfileSerializer, UserSerializer
 
 
 class SignupView(APIView):
@@ -19,6 +19,15 @@ class SignupView(APIView):
             return Response({"message":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 class ProfileView(APIView):
+    def get(self, request, user_id):
+        """사용자의 프로필을 받아 보여줍니다."""
+        profile = get_object_or_404(User, id=user_id)
+        if request.user.email == profile.email:                
+            serializer = ProfileSerializer(profile)    
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"message":"권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+        
     def put(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
         if request.user == user:
@@ -29,7 +38,7 @@ class ProfileView(APIView):
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
+            return Response({"message":"권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
         
 
 class LoginView(TokenObtainPairView):
@@ -38,7 +47,6 @@ class LoginView(TokenObtainPairView):
     DRF의 JWT 토큰 인증 로그인 방식에 기본 제공되는 클래스 뷰를 커스터마이징하여 재정의합니다.
     """
     serializer_class = LoginSerializer
-            return Response({"message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class FollowView(APIView):
@@ -46,13 +54,13 @@ class FollowView(APIView):
         """사용자가 다른 사용자를 팔로우하고 언팔로우합니다."""
 
         # user_id 사용해서 팔로우 사용자 가져오기
-        followee = get_object_or_404(User, id=user_id)
-        follower = request.user  # 팔로우 요청을 보내는 사용자
-        if follower in follower.followers.all():  # 사용자를 이미 팔로우한다면
+        user = get_object_or_404(User, id=user_id)
+        me = request.user  # 팔로우 요청을 보내는 사용자
+        if me in user.followers.all():  # 사용자를 이미 팔로우한다면
             # followee 사용자 목록에서 follower remove로 제거하기
-            followee.followers.remove(follower)
+            user.followers.remove(me)
             return Response("unfollow 했습니다.", status=status.HTTP_200_OK)
         else:
             # 아니면 followee 사용자 목록에 follower 추가
-            followee.followers.add(follower)
+            user.followers.add(me)
             return Response("follow 했습니다.", status=status.HTTP_200_OK)
