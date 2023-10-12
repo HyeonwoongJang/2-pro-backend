72decfb7dfa6046dab66d16f6e39b16aa6398fa4
EunjinWoo <esther0904@naver.com>
2023-10-12 13:25:20 +0900

    Feat. Password Update

--- users/views.py
+++ users/views.py
@@ -20,6 +20,8 @@ from .tasks import test_task
 from django.http import HttpRequest
 from rest_framework import views
 
+from django.contrib.auth.hashers import check_password
+
 
 class Test(views.APIView):
     def get(self, request: HttpRequest):
@@ -87,12 +89,27 @@ class ProfileView(APIView):
     def put(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
         if request.user == user:
-            serializer = UserSerializer(user, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            if 'present_pw' in request.data: # 비밀번호 변경할 때
+                if check_password(request.data['present_pw'], user.password) == True: # 현재 비밀번호가 일치하는지 확인.
+                    if request.data['password'] == request.data['password_check']: # 새로 입력한 비밀번호와 비밀번호 확인이 일치하는지 확인.
+                        serializer = UserSerializer(user, data=request.data, partial=True)
+                        if serializer.is_valid():
+                            serializer.save()
+                            return Response(serializer.data, status=status.HTTP_200_OK)
+                        else:
+                            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                    else:
+                        return Response({"message":"비밀번호가 일치하지 않습니다. 다시 입력하세요."}, status=status.HTTP_403_FORBIDDEN)
+                else:
+                    return Response({"message":"현재 비밀번호를 확인하세요."}, status=status.HTTP_403_FORBIDDEN)
+
+            else: # 비밀번호는 변경하지 않을 때
+                serializer = UserSerializer(user, data=request.data, partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({"message":"권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
         
